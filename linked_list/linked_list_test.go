b59c100deb5231a022936c66f94200188d04e624
Mateus Ribeiro <mateus.ribe.sva@gmail.com>
2023-09-29 19:40:56 -0300

    tests: added tests for linked lists

--- linked_list/linked_list_test.go
+++ linked_list/linked_list_test.go
@@ -80,11 +80,12 @@ func TestLinkedList(t *testing.T){
 	require.True(t, l.Empty())
 
 	for i := 0; i < 5; i++ {
-		l.PushFront(int64(i+1))
+		l.PushBack(int64(i+1))
 	}
 
 	node := l.Find(int64(5))
 	require.Equal(t, int64(5), node.Value)
+
 
 	node = l.Find(int64(1))
 	require.Equal(t, int64(1), node.Value)
@@ -97,17 +98,50 @@ func TestLinkedList(t *testing.T){
 
 	err := l.Delete(int64(3))
 	require.NoError(t, err)
+	require.Equal(t, int64(4), l.Head.Next.Next.Value)
 
 	err = l.Delete(int64(5))
 	require.NoError(t, err)
+	require.Equal(t, int64(4), l.Tail.Value)
 
 	err = l.Delete(int64(1))
 	require.NoError(t, err)
+	require.Equal(t, int64(2), l.Head.Value)
 
 	err = l.Delete(int64(54))
 	require.Error(t, err)
 
 	l.Delete(int64(2))
+	require.Equal(t, int64(4), l.Tail.Value)
+	require.Equal(t, int64(4), l.Head.Value)
+	l.Delete(int64(4))
+	require.True(t, l.Empty())
+
+	err = l.Delete(int64(4))
+	require.Error(t, err)
+
+	for i := 0; i < 5; i++ {
+		l.PushFront(int64(i+1))
+	}
+
+	err = l.Delete(int64(3))
+	require.NoError(t, err)
+	require.Equal(t, int64(2), l.Head.Next.Next.Value)
+
+	err = l.Delete(int64(5))
+	require.NoError(t, err)
+	require.Equal(t, int64(4), l.Head.Value)
+
+	err = l.Delete(int64(1))
+	require.NoError(t, err)
+	require.Equal(t, int64(2), l.Tail.Value)
+
+	err = l.Delete(int64(54))
+	require.Error(t, err)
+
+	l.Delete(int64(2))
+	require.Equal(t, int64(4), l.Tail.Value)
+	require.Equal(t, int64(4), l.Head.Value)
 	l.Delete(int64(4))
 	require.True(t, l.Empty())
 
